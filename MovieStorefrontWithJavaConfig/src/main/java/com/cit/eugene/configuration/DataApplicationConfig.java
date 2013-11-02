749c94498dcf731771b0f37199edf225d33a38f7
Eugene Bell <ebell@animalhealthmonitoring.com>
2013-11-02 16:13:15 +0000

    data app config

--- MovieStorefrontWithJavaConfig/src/main/java/com/cit/eugene/configuration/DataApplicationConfig.java
+++ MovieStorefrontWithJavaConfig/src/main/java/com/cit/eugene/configuration/DataApplicationConfig.java
@@ -1,11 +1,18 @@
 package com.cit.eugene.configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 //@AnnotationDrivenConfig
@@ -21,5 +28,37 @@ public class DataApplicationConfig {
 		ds.setPassword("password");
 		return ds;
 	}
+	
+	@Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(jpaVendorAdapter);
+        //factory.setPackagesToScan("com.satesh.springjsfjpa.entity");
+        factory.setDataSource(dataSource());
+        factory.setPersistenceUnitName("video-unit");
+        //factory.afterPropertiesSet();
+        //factory.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+       return factory;
+    }
 
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+    	HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(Boolean.FALSE);
+        vendorAdapter.setShowSql(Boolean.TRUE);
+        vendorAdapter.setDatabase(Database.MYSQL);
+        return vendorAdapter;
+    }
+//    
+//    @Bean
+//    public EntityManager entityManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+//        return entityManagerFactory.get;
+//    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
+
+    
 }
